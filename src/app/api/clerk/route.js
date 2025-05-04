7ae1082b9054363ed18ea26b6925195f151726a0
vgdhieu0602 <vg.dhieu0602@gmail.com>
2025-05-04 19:07:35 +0700

    Webhook Clerk

--- src/app/api/clerk/route.js
+++ src/app/api/clerk/route.js
@@ -1,3 +1,46 @@
 import { Webhook } from "svix";
 import connectDB from "@/app/config/db";
 import User from "@/app/models/User";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function POST(req) {
+  const wh = new Webhook(process.env.SIGNING_SECRET);
+  const headerPayload = await headers;
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id"),
+    "svix-signature": headerPayload.get("svix-signature"),
+  };
+
+  //Get the payload and verify it
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+  const { data, type } = wh.verify(body, svixHeaders);
+
+  //Prepare the user data to be saved in the database
+  const userData = {
+    _id: data.id,
+    email: data.email_addresses[0].email_address,
+    name: `${data.first_name} ${data.last_name}`,
+    image: data.image_url,
+  };
+
+  await connectDB();
+
+  switch (type) {
+    case "user.created":
+      await User.create(userData);
+      break;
+    case "user.updated":
+      await User.findByIdAndUpdate(data.id, userData);
+      break;
+    case "user.deleted":
+      await User.findByIdAndDelete(data.id);
+      break;
+
+    default:
+      break;
+  }
+
+  return NextRequest.json({message: "Event received"})
+}
