7a1b4720cfcf07c926626a5a8fde62a5add6b8f0
vgdhieu0602 <vg.dhieu0602@gmail.com>
2025-05-09 12:43:51 +0700

    initial seed DB

---
+++ src/app/script/loadDB.ts
@@ -0,0 +1,7 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import OpenAI from "openai";
+
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import "dotenv/config";
